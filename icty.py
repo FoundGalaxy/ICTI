aff3ed463241d56af8b05e529a70284b24f475ab
FoundGalaxy <luis-gomes-lmg@hotmail.com>
2024-01-29 12:46:10 -0600

    changes from vsc

--- icty.py
+++ icty.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/conversor.html')
+def conversor():
+    return render_template('conversor.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 4000, debug = True)
