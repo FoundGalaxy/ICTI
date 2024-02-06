27abcad4cbe6e175f590ddd326e9cded6cc7670b
FoundGalaxy <luis-gomes-lmg@hotmail.com>
2024-02-06 10:31:11 -0600

    Sistema de conversion corregida

--- icti.py
+++ icti.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, Response
+from flask import Flask, render_template, request, redirect, url_for, flash, session, Response, send_from_directory, make_response
 import pandas as pd
 import re
 import os 
@@ -30,6 +30,9 @@ def registros():
 def upload():
     if 'file' not in request.files:
         return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
     if file:
         #Guardamos el archivo en una carpeta local
         filename = file.filename
@@ -39,10 +42,11 @@ def upload():
         #Leemos el archivo .dat con pandas
         df = pd.read_csv(file_path, sep='\t')
 
-        #Convertimos el DataFrame a un archivo .xls
+        #Convertimos el DataFrame a un archivo .xls usando ExcelWriter
         xls_filename = filename.replace('.dat', '.xls')
         xls_file_path = os.path.join('downloads', xls_filename)
-        df.to_excel(xls_file_path, index=False)
+        with pd.ExcelWriter(xls_file_path, engine='xlsxwriter') as writer:            
+            df.to_excel(writer, index=False)
 
         #Eliminamos el archivo .dat
         os.remove(file_path)
@@ -50,11 +54,19 @@ def upload():
         #Devolvemos la descarga del archivo xls
         return redirect(url_for('download', filename=xls_filename))
     
-@app.route('/download/<filename>')
+@app.route('/downloads/<filename>')
 def download(filename):
     downloads_folder = 'downloads'
-    return redirect(url_for('static', filename=os.path.join(downloads_folder, filename)),code=301)
+    file_path = os.path.abspath(os.path.join(downloads_folder, filename))
+    #return send_from_directory(directory=os.path.abspath(downloads_folder), path=filename)
+    
+    #Enviamos el archivo para su descarga
+    response = send_from_directory(directory=downloads_folder, path=filename)
 
+    #eliminamos el archivo local despues de su descarga
+    os.remove(file_path)
+
+    return response
 if __name__ == '__main__':
 
     if not os.path.exists('uploads'):
