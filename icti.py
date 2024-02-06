f4d81cafa59e5cdab0064a4526b65d3888f36919
FoundGalaxy <luis-gomes-lmg@hotmail.com>
2024-02-06 12:26:23 -0600

    new branch 4 testdevelop

--- icti.py
+++ icti.py
@@ -18,14 +18,6 @@ def index():
 def conversor():
     return render_template('conversor.html')
 
-@app.route('/asistencias.html')
-def asistencias():
-    return render_template('asistencias.html')
-
-@app.route('/registros.html')
-def registros():
-    return render_template('registros.html')
-
 @app.route('/upload', methods=['POST'])
 def upload():
     if 'file' not in request.files:
